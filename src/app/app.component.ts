6f2a5be7ec920bc701a847b5174b7829be749b39
unknown <victorbr8@outlook.com>
2025-02-28 18:16:47 -0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

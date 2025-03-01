9bc01551351bc5b3b74e0acd5c256a45004917d5
unknown <victorbr8@outlook.com>
2025-03-01 16:50:46 -0300

    responsiva

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule], // ✅ Importa o ReactiveFormsModule aqui
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent {
-  results = [];
+export class FormsComponent implements OnInit {
+  results: any[] = [];
+  formGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: [''],
+      showClosed: [false],
+    });
+  }
+
+  OnSubmit(): void {
+    console.log(this.formGroup.value) ;
+
+  }
+
+  OnClear(): void {
+   this.formGroup.reset();
+  }
 }
